34744adc9ae05c0858ddb1fa0bdc01fc9973d5c7
Rafaella Cintra <60405697+rafaellacintra@users.noreply.github.com>
2022-06-22 03:09:26 -0300

    refactor: describe request

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { gql, useQuery } from '@apollo/client'
+import Event from './pages/Event'
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons{
-      id
-      title
-    }
-  }
-`
-interface lesson {
-  id: string;
-  title: string;
-}
 export default function App () {
-  const { data } = useQuery<{lessons:lesson[]}>(GET_LESSONS_QUERY)
-  console.log(data)
-
   return (
-    <ul>
-      {data?.lessons.map((lesson, idx) => (
-        <li key={idx}>{lesson?.title}</li>
-      ))}
-    </ul>
+    <Event/>
   )
 }
